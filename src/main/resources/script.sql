776ec5b6a3774d7945c1acb0ef93db23e0e807f0
Zakaria Imtiaz <imtiaz71985@gmail.com>
2024-06-03 12:18:07 +0600

    db script
    necessary script

---
+++ src/main/resources/script.sql
@@ -0,0 +1,39 @@
+-- Create database
+CREATE DATABASE weatherapp;
+
+-- Drop table if it already exists
+DROP TABLE IF EXISTS public.users;
+
+-- Create table
+CREATE TABLE public.users
+(
+    id        SERIAL PRIMARY KEY,
+    username  VARCHAR(255) NOT NULL,
+    password  VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    CONSTRAINT user_username_unique UNIQUE (username)
+);
+
+-- Insert sample data
+INSERT INTO public.users (username, password, full_name)
+VALUES ('john_doe', 'password123', 'John Doe'),
+       ('jane_smith', 'pass456', 'Jane Smith'),
+       ('admin', 'admin123', 'Admin User');
+
+
+-- Drop table if it already exists
+DROP TABLE IF EXISTS public.location;
+-- Create table
+CREATE TABLE public."location"
+(
+    id        serial4      NOT NULL,
+    "name"    varchar(255) NOT NULL,
+    admin1    varchar(255) NULL,
+    admin2    varchar(255) NULL,
+    admin3    varchar(255) NULL,
+    latitude  float8       NOT NULL,
+    longitude float8       NOT NULL,
+    user_id   int4         NOT NULL,
+    CONSTRAINT location_pkey PRIMARY KEY (id)
+);
+
